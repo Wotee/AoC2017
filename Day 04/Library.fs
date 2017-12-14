ccc263add22ce5e44276ce8ea34433ddb5307291
Wote <valtteri6@msn.com>
2017-12-14 17:51:59 +0200

    Implemented phase 2

--- Day 04/Library.fs
+++ Day 04/Library.fs
@@ -2,25 +2,18 @@ module Day04
   let phase1 (input:array<string>) : int=
     Array.fold (fun acc (x : string) ->
       let y = x.Split ' '
-      acc + (
-        if Array.distinct y = y then 1
-        else 0
-      )
+      acc + (if Array.distinct y = y then 1 else 0)
     ) 0 input
 
 
   let phase2 (input:array<string>)=
     Array.fold (fun acc (x : string) ->
-      let y = x.Split[|' '|]
-      acc + (
-        0
-        //Array.permute y
-        // Check permutations
-      )
+      let y = x.Split ' '
+      let Y = Array.map (fun (z:string) -> z |> Seq.sort |> System.String.Concat) y |> Array.distinct
+      acc + (if Array.length y = Array.length Y then 1 else 0)
     ) 0 input
 
   let run() =
     let input = System.IO.File.ReadAllLines("..\Day 04\input.txt")
-
     printfn "Phase 1: %i" (phase1 input)
     printfn "Phase 2: %i" (phase2 input)
