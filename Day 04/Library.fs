c0b587f0c93ad5a93d17ab2031f011b8a810be97
Wote <valtteri6@msn.com>
2017-12-14 20:31:09 +0200

    Streamlined even more

--- Day 04/Library.fs
+++ Day 04/Library.fs
@@ -5,7 +5,7 @@ module Day04
     let parsed = Array.map (fun (z:string) -> z |> Seq.sort |> System.String.Concat) input |> Array.distinct
     (if Array.length input = Array.length parsed then 1 else 0)
 
-  let phase (input:array<string>) func=
+  let phase input func=
     Array.fold (fun acc (x:string) ->
       let y = x.Split ' '
       acc + func y
