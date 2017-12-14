3157047ba3eb2903085f40d3d3a119473ff72815
Wote <valtteri6@msn.com>
2017-12-14 19:53:58 +0200

    Day 5

--- Day 05/Library.fs
+++ Day 05/Library.fs
@@ -1,19 +1,17 @@
-// Valtteri Tarvus
-// Advent of Code 2017 http://adventofcode.com/2017
-// Challenge day 5 - 
-
-// Input for this software is 
-// Phase 1 calculates 
-// Phase 2 calculates 
-
 module Day05
-  let phase1 (input:string)=
-     0
+  let func1 (input:int) : int = input + 1
+  let func2 (input:int) : int = (if input < 3 then (+) else (-)) input 1
 
-  let phase2 (input:string)=
-    0
+  let phase input func =
+    let rec takeStep (input:array<int>) step index =
+      let newLoc = input.[index] + index
+      input.[index] <- func input.[index]
+      if newLoc >= 0 && newLoc < (Array.length input) then
+        takeStep input (step+1) newLoc
+      else step+1
+    takeStep input 0 0
 
   let run() =
-    let input = System.IO.File.ReadAllText("..\Day 02\input.txt")
-    printfn "Phase 1: %i" (phase1 input)
-    printfn "Phase 2: %i" (phase2 input)
+    let input = System.IO.File.ReadAllLines("..\Day 05\input.txt") |> Seq.map int
+    printfn "Phase 1: %i" (phase (Seq.toArray input) func1)
+    printfn "Phase 2: %i" (phase (Seq.toArray input) func2)
---
+++ Day 05/README.md
@@ -0,0 +1,7 @@
+# [Advent of Code 2017 Day 5 - A Maze of Twisty Trampolines, All Alike](http://adventofcode.com/2017/day/5)
+## Input
+Input for this software is a multiline series of positive and negative integers
+## Phase 1
+Phase 1 calculates the count of steps needed to get out of the input array of data. After each step, the element the element that was processed is incremented by 1
+## Phase 2
+Phase 2 calculates the count of steps needed to get out of the input array of data. After each step, the element the element that was processed is incremented by 1 if it is smaller than 3, otherwise decrement it by 1
